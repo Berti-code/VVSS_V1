0db957c1aa17e2726b4c9a43894aae207ed11e17
Berti-code <59232000+Berti-code@users.noreply.github.com>
2022-03-26 18:28:55 +0200

    TestStudent Class created.

---
+++ src/test/java/TestStudent.java
@@ -0,0 +1,39 @@
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+public class TestStudent {
+    private Validator<Student> studentValidator;
+    private Validator<Tema> temaValidator;
+    private Validator<Nota> notaValidator;
+
+    private StudentXMLRepository fileRepository1;
+    private TemaXMLRepository fileRepository2;
+    private NotaXMLRepository fileRepository3;
+
+    TestStudent(){
+        this.studentValidator = new StudentValidator();
+        this.temaValidator = new TemaValidator();
+        this.notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    @Test
+    public void TestAddStudent(){
+
+    }
+}
