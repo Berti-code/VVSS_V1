38b9405b99621681e2663d59c69bfe94f4ba0225
Berti-code <59232000+Berti-code@users.noreply.github.com>
2022-03-27 16:57:00 +0300

    TestStudent Class tests added.

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -3,11 +3,11 @@ package service;
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
-import service.Service;
 import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
@@ -32,32 +32,32 @@ public class ServiceTest {
 
     @Test
     public void testAddStudent_InvalidGroup110_Fails() {
-        Assertions.assertEquals(1, this.service.saveStudent("student1", "George1", 110));
+        Assert.assertEquals(1, this.service.saveStudent("student1", "George1", 110));
     }
 
     @Test
     public void testAddStudent_ValidGroup111_Passes() {
-        Assertions.assertEquals(0, this.service.saveStudent("student2", "George2", 111));
+        Assert.assertEquals(0, this.service.saveStudent("student2", "George2", 111));
     }
 
     @Test
     public void testAddStudent_InvalidGroup109_Fails() {
-        Assertions.assertEquals(1, this.service.saveStudent("student3", "George3", 109));
+        Assert.assertEquals(1, this.service.saveStudent("student3", "George3", 109));
     }
 
     @Test
     public void testAddStudent_InvalidGroup939_Fails() {
-        Assertions.assertEquals(1, this.service.saveStudent("student4", "George4", 939));
+        Assert.assertEquals(1, this.service.saveStudent("student4", "George4", 939));
     }
 
     @Test
     public void testAddStudent_ValidGroup938_Passes() {
-        Assertions.assertEquals(1, this.service.saveStudent("student5", "George5", 938));
+        Assert.assertEquals(1, this.service.saveStudent("student5", "George5", 938));
     }
 
     @Test
     public void TestAddStudent_ValidName_Passes() {
-        Assertions.assertEquals(0, this.service.saveStudent("11", "ValidName", 111));
+        Assert.assertEquals(0, this.service.saveStudent("11", "ValidName", 111));
     }
 
     @Test
@@ -67,21 +67,21 @@ public class ServiceTest {
 
     @Test
     public void TestAddStudent_Null_Fails() {
-        Assertions.assertEquals(1, this.service.saveStudent("13", null, 937));
+        Assert.assertEquals(1, this.service.saveStudent("13", null, 937));
     }
 
     @Test
     public void TestAddStudent_ID_Passes() {
-        Assertions.assertEquals(0, this.service.saveStudent("11", "ValidName", 111));
+        Assert.assertEquals(0, this.service.saveStudent("11", "ValidName", 111));
     }
 
     @Test
     public void TestAddStudent_EmptyID_Fails() {
-        Assertions.assertEquals(1, this.service.saveStudent("", "ValidName", 937));
+        Assert.assertEquals(1, this.service.saveStudent("", "ValidName", 937));
     }
 
     @Test
     public void TestAddStudent_NullID_Fails() {
-        Assertions.assertEquals(1, this.service.saveStudent(null, "ValidName", 937));
+        Assert.assertEquals(1, this.service.saveStudent(null, "ValidName", 937));
     }
 }
