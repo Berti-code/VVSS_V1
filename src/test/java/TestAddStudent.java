960bdfd9f404a0fcaddb4c0f8197d2576e949058
Berti-code <59232000+Berti-code@users.noreply.github.com>
2022-03-27 14:02:45 +0300

    TestStudent Class tests added.

--- src/test/java/TestAddStudent.java
+++ src/test/java/TestAddStudent.java
@@ -13,56 +13,73 @@ import validation.Validator;
 import org.junit.jupiter.api.Assertions;
 
 public class TestAddStudent {
-    private Validator<Student> studentValidator;
-    private Validator<Tema> temaValidator;
-    private Validator<Nota> notaValidator;
 
-    private StudentXMLRepository fileRepository1;
-    private TemaXMLRepository fileRepository2;
-    private NotaXMLRepository fileRepository3;
-
-    private Service service;
+    private final Service service;
 
     TestAddStudent() {
-        this.studentValidator = new StudentValidator();
-        this.temaValidator = new TemaValidator();
-        this.notaValidator = new NotaValidator();
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
 
-        this.fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        this.fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        this.fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
 
         service = new Service(fileRepository1, fileRepository2, fileRepository3);
     }
 
-    // id si grupa 110, 111, 109, 939, 938, 937
-    // id - valid, null, empty string
-    // nume - valid, null, empty string
-    // grupa - 111, 110, 109
-    // grupa - 939, 938, 937
-
     @Test
     public void testAddStudent_InvalidGroup110_DoesntPass() {
-        Assertions.assertEquals(this.service.saveStudent("student1", "George1", 110), 1);
+        Assertions.assertEquals(1, this.service.saveStudent("student1", "George1", 110));
     }
 
     @Test
     public void testAddStudent_ValidGroup111_Passes() {
-        Assertions.assertEquals(this.service.saveStudent("student2", "George2", 111), 0);
+        Assertions.assertEquals(0, this.service.saveStudent("student2", "George2", 111));
     }
 
     @Test
     public void testAddStudent_InvalidGroup109_DoesntPass() {
-        Assertions.assertEquals(this.service.saveStudent("student3", "George3", 109), 1);
+        Assertions.assertEquals(1, this.service.saveStudent("student3", "George3", 109));
     }
 
     @Test
     public void testAddStudent_InvalidGroup939_DoesntPass() {
-        Assertions.assertEquals(this.service.saveStudent("student4", "George4", 939),1);
+        Assertions.assertEquals(1, this.service.saveStudent("student4", "George4", 939));
     }
 
     @Test
     public void testAddStudent_ValidGroup938_Passes() {
-        Assertions.assertEquals(this.service.saveStudent("student5", "George5", 938), 1);
+        Assertions.assertEquals(1, this.service.saveStudent("student5", "George5", 938));
+    }
+
+    @Test
+    public void TestAddStudent_ValidName_Passes() {
+        Assertions.assertEquals(0, this.service.saveStudent("11", "ValidName", 111));
+    }
+
+    @Test
+    public void TestAddStudent_EmptyName_Fails() {
+        Assertions.assertEquals(1, this.service.saveStudent("12", "", 937));
+    }
+
+    @Test
+    public void TestAddStudent_Null_Fails() {
+        Assertions.assertEquals(1, this.service.saveStudent("13", null, 937));
+    }
+
+    @Test
+    public void TestAddStudent_ID_Passes() {
+        Assertions.assertEquals(0, this.service.saveStudent("11", "ValidName", 111));
+    }
+
+    @Test
+    public void TestAddStudent_EmptyID_Fails() {
+        Assertions.assertEquals(1, this.service.saveStudent("", "ValidName", 937));
+    }
+
+    @Test
+    public void TestAddStudent_NullID_Fails() {
+        Assertions.assertEquals(1, this.service.saveStudent(null, "ValidName", 937));
     }
 }
