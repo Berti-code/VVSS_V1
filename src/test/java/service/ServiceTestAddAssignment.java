34cdf5c1edf3d6c64ab879bac10d1482408afc88
Berti-code <59232000+Berti-code@users.noreply.github.com>
2022-04-09 16:30:17 +0300

    AddAssignment NullId and ValidId

--- src/test/java/service/ServiceTestAddAssignment.java
+++ src/test/java/service/ServiceTestAddAssignment.java
@@ -3,12 +3,15 @@ package service;
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
+import org.apache.commons.collections4.IterableUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import validation.*;
+
+import java.util.Collections;
 
 public class ServiceTestAddAssignment{
     Validator<Student> studentValidator = new StudentValidator();
@@ -22,22 +25,33 @@ public class ServiceTestAddAssignment{
     private final Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
 
     @Test
-    public void addAssignment_UniqueId_Passed() {
+    public void addAssignment_UniqueId_Added() {
         Assert.assertEquals(this.service.saveTema("tema1", "tema de laborator", 4, 7), 1);
     }
 
     @Test
-    public void addAssignment_ExistingId_Failed() {
+    public void addAssignment_ExistingId_NotAdded() {
         Assert.assertEquals(this.service.saveTema("tema1", "tema de lab", 3, 5), 1);
     }
 
     @Test
-    public void addAssignment_NullId_Failed() {
+    public void addAssignment_NullId_NotAdded() {
+        Iterable<Tema> elems = fileRepository2.findAll();
         try {
             Tema tema = new Tema("", "tema lab", 6, 4);
-            Tema result = fileRepository2.save(tema);
+            fileRepository2.save(tema);
         } catch(ValidationException e) {
-           Assert.assertEquals(e.toString(), "ID invalid! \n");
+            Assert.fail();
         }
+        Iterable<Tema> elemsNew = fileRepository2.findAll();
+        Assert.assertEquals(IterableUtils.size(elems), IterableUtils.size(elemsNew));
+    }
+
+    @Test
+    public void addAssignment_ValidId_Added() {
+        Tema tema = new Tema("1", "tema lab", 6, 4);
+        fileRepository2.save(tema);
+        Tema elem = fileRepository2.findOne("1");
+        Assert.assertEquals(elem.getID(), tema.getID());
     }
 }
